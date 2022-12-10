72000dedef51e212da17145ab2c55175670ceca3
Bob Oyier <boboyier92@gmail.com>
2022-12-10 10:39:21 +0300

    "completed Moringa School's array map method OPTIONAL lab"

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
+	"what does the this keyword mean?",
+	"What is the Constructor OO pattern?",
+	"implementing Blockchain Web API",
+	"The Test Driven Development Workflow",
+	"What is NaN and how Can we Check for it",
+	"What is the difference between stopPropagation and preventDefault?",
+	"Immutable State and Pure Functions",
+	"what is the difference between == and ===?",
+	"what is the difference between event capturing and bubbling?",
+	"what is JSONP?",
 ];
 
 const titleCased = () => {
-  return tutorials
-}
+let test = tutorials.map((element) =>
+	element
+		.split(" ")
+		.map((element) => element.slice(0, 1)[0].toUpperCase() + element.slice(1))
+		.join(" ")
+);
+	return test
+
+};
+
+// console.log(titleCased());
